8f87668ce5a76eeabb4964e1d8d691b4d33a3353
end <72604018+laok233@users.noreply.github.com>
2023-04-09 21:33:46 +0200

    lol lol lol lol lol lol

--- scripts/title3.js
+++ scripts/title3.js
@@ -1,6 +1,6 @@
-const music = loadMusic("title3.ogg");
-Events.on(WorldLoadEvent, e => {
-  Vars.control.sound.stop();
-  music.stop();
-  music.play();
-});
+// const music = loadMusic("title3.ogg");
+// Events.on(WorldLoadEvent, e => {
+//   Vars.control.sound.stop();
+//   music.stop();
+//   music.play();
+// });
